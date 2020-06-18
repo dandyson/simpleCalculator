1aa5e0181c232870a1b3515ec6f026d07d2c5d59
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-06-18 20:51:17 +0100

    Updated styles and functionality

--- Readme.md
+++ Readme.md
@@ -11,3 +11,17 @@
 4. When a number or operator button is clicked, get the inner text of that button and display it in the display at the top.
 
 5. When the user presses equals, check what math operator was used, split the 2 numbers either side into separate variables and calculate the result based on which operator was used.
+
+## Route 1:
+
+1. I tried to make the calculations by using multiple if statements - So if the display result included any of the operators, split the 2 numbers either side of the operator into separate variables, then evaluate them with the operator in question.
+
+#This is not right, as this will only count for 2 numbers and the code is way too much for something simple!
+
+## Route 2 (The winning solution!):
+
+1. Found the eval() function - Changed the text field to an input so the user can write their own calculations. I used eval() on the value of the input when the equals button is pressed, the displayed the result in the input value.
+
+## Extras
+
+- Used Tilt.js for the calculator and depth effect on buttons (http://gijsroge.github.io/tilt.js/)
--- script.js
+++ script.js
@@ -3,61 +3,100 @@ const operators = document.querySelectorAll('.operator');
 const equals = document.querySelector('.equals');
 const clearAll = document.querySelector('.clearAll');
 let textDisplay = document.querySelector('#textDisplay');
-let calcText = '';
-let firstNumber;
-let nextNumber;
 
-function number() {
-    calcText += this.textContent;
-    textDisplay.textContent = calcText;
-}
-
-function operatorFunc() {
-    calcText + this.textContent;
+function addToDisplay() {
+    textDisplay.value += this.textContent;
 }
 
 function calculate() {
-    const add = calcText.includes('+');
-    const subtract = calcText.includes('-');
-    const times = calcText.includes('x');
-    const divide = calcText.includes('÷');
-
-    if (add) {
-        const [previous, next] = calcText.split('+');
-        let answer = parseFloat(previous) + parseFloat(next);
-        textDisplay.textContent = answer;
-    } else if (subtract) {
-        const [previous, next] = calcText.split('-');
-        let answer = previous - next;
-        textDisplay.textContent = answer;
-    } else if (times) {
-        const [previous, next] = calcText.split('x');
-        let answer = previous * next;
-        textDisplay.textContent = answer;
-    } else if (divide) {
-        const [previous, next] = calcText.split('÷');
-        let answer = previous / next;
-        textDisplay.textContent = answer;
-    }
+    let answer = eval(textDisplay.value);
+    textDisplay.value = answer;
 }
 
-buttons.forEach(button => {
-    button.addEventListener('click', number);
+function clear() {
+    textDisplay.value = '';
 }
-);
-
-operators.forEach(operator => {
-    operator.addEventListener('click', operatorFunc);
-}
-);
 
 equals.addEventListener('click', calculate);
+clearAll.addEventListener('click', clear);
 
-clearAll.addEventListener('click', function () {
-    calcText = '';
-    textDisplay.textContent = '';
-    storeNumber = '';
+buttons.forEach(button => {
+    button.addEventListener('click', addToDisplay);
 })
+
+// //ATTEMPT 1
+
+// // let calcText = '';
+// // let firstNumber;
+// // let nextNumber;
+
+// //Add the text content of this to calcText, which either 
+// //assigns just that if its empty, or it will add a new 
+// //number with the previous
+// function number() {
+//     calcText += this.textContent;
+//     textDisplay.textContent = calcText;
+// }
+
+// //Adds operator in question to the calcText variable
+// function operatorFunc() {
+//     calcText + this.textContent;
+// }
+
+// //Depending what operator is used, it is assigned accordingly
+// //to a variavle
+// function calculate() {
+//     const add = calcText.includes('+');
+//     const subtract = calcText.includes('-');
+//     const times = calcText.includes('x');
+//     const divide = calcText.includes('÷');
+
+//     //If the display matches any of the variables above,
+//     //Split the string and calculate the result, then
+//     //Display that result
+//     if (add) {
+//         const [previous, next] = calcText.split('+');
+//         let answer = parseFloat(previous) + parseFloat(next);
+//         textDisplay.textContent = answer;
+//     } else if (subtract) {
+//         const [previous, next] = calcText.split('-');
+//         let answer = previous - next;
+//         textDisplay.textContent = answer;
+//     } else if (times) {
+//         const [previous, next] = calcText.split('x');
+//         let answer = previous * next;
+//         textDisplay.textContent = answer;
+//     } else if (divide) {
+//         const [previous, next] = calcText.split('÷');
+//         let answer = previous / next;
+//         textDisplay.textContent = answer;
+//     }
+// }
+
+// //For every number button, listen for a click and run
+// //the number function
+// buttons.forEach(button => {
+//     button.addEventListener('click', number);
+// }
+// );
+
+// //For every operator button, listen for a click and 
+// //run the operatorFunc function
+// operators.forEach(operator => {
+//     operator.addEventListener('click', operatorFunc);
+// }
+// );
+
+// //When the equals sign is clicked, run the calculate function
+// equals.addEventListener('click', calculate);
+
+// //When the clearAll button is clicked, clear all variables 
+// //back to empty
+// clearAll.addEventListener('click', function () {
+//     calcText = '';
+//     textDisplay.textContent = '';
+//     storeNumber = '';
+// })
 
 
 
